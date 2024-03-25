fe6286275219c46aa3f085cc92116d4c73769fe7
Koalaphant <andrew.wardjones@icloud.com>
2024-03-25 11:39:45 +0000

    fixing safe area view

--- components/header.jsx
+++ components/header.jsx
@@ -1,29 +1,13 @@
 import React from "react";
-import {
-  Text,
-  View,
-  StyleSheet,
-  Button,
-  Image,
-  TouchableOpacity,
-} from "react-native";
+import { Text, View, StyleSheet, Image } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
 const Header = () => {
   const navigation = useNavigation();
   return (
     <>
-      <View style={styles.container}>
-        <Image
-          source={require("../assets/journalbanner.jpg")}
-          style={styles.banner}
-        />
-        <View style={styles.overlay}>
-          <View style={styles.header}>
-            <Text style={styles.title}>Pocket Journal</Text>
-            <Text style={styles.subHeading}>A travel journey experience</Text>
-          </View>
-        </View>
+      <View>
+        <Text>PocketJournal</Text>
       </View>
     </>
   );
@@ -33,48 +17,6 @@ const styles = StyleSheet.create({
   container: {
     marginTop: 0,
     marginBottom: 0,
-  },
-  header: {
-    borderRadius: 20,
-    marginVertical: 10,
-    alignSelf: "center",
-    padding: 8,
-  },
-  title: {
-    color: "#D76778",
-    width: "auto",
-    fontSize: 40,
-    fontWeight: "bold",
-    marginVertical: 15,
-    alignSelf: "center",
-  },
-  subHeading: {
-    alignSelf: "center",
-    color: "#D76778",
-    width: "auto",
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  navbar: {
-    flexDirection: "row",
-    alignContent: "center",
-    justifyContent: "space-around",
-    marginTop: 0,
-    backgroundColor: "#D76778",
-  },
-  buttonText: {
-    color: "#fff",
-    fontSize: 16,
-    fontWeight: "bold",
-    paddingVertical: 5,
-  },
-  banner: {
-    width: "auto",
-    height: 150,
-  },
-  overlay: {
-    ...StyleSheet.absoluteFillObject,
-    backgroundColor: "rgba(255, 237, 223,0.7)",
   },
 });
 
