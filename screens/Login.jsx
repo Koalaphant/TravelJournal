fe192f6c804c92837c39c83d515a9f32fd27da27
Kelly <kellyhowes83@gmail.com>
2024-03-26 14:09:56 +0000

    Link login

--- screens/Login.jsx
+++ screens/Login.jsx
@@ -2,21 +2,36 @@ import { View, Text, StyleSheet, Button, TextInput, ActivityIndicator, KeyboardA
 import React, { useState } from "react";
 import { FIREBASE_AUTH } from "../services/config";
 import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
+import {collection, addDoc } from "firebase/firestore";
+import {db} from '../services/config.js'
 const auth = FIREBASE_AUTH;
+
 
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
   const [loading, setLoading] = useState(false);
 
   const auth = FIREBASE_AUTH;
+  const user = FIREBASE_AUTH.currentUser
+  console.log(user, "THIS")
   const signIn = async () => {
     setLoading(true)
   
     try{
       const response = await signInWithEmailAndPassword(auth, email, password);
-      console.log(response)
+      console.log(response, " <<LOG IN")
+      await addDoc(collection(db, "users", user), {
+        username: username,
+        email: email
+      }).then(() =>{
+        console.log('data submitted')
+      }).catch((error) =>{
+        console.log(error)
+      })
+     
     }catch(error){
       console.log(error)
     } finally{
@@ -29,7 +44,8 @@ const Login = () => {
   
     try{
       const response = await createUserWithEmailAndPassword(auth, email, password);
-      console.log(response)
+      console.log(response, " <<SIGN UP")
+      
       alert('Check your email')
     }catch(error){
       console.log(error)
@@ -42,6 +58,7 @@ const Login = () => {
   return (
     <View style = {styles.container}>
       <KeyboardAvoidingView behaviour='padding'>
+      <TextInput value={username} onChangeText={setUsername} placeholder="Name" style={styles.input}/>
       <TextInput value={email} onChangeText={setEmail} placeholder="Email" style={styles.input}/>
       <TextInput value={password} secureTextEntry={true} onChangeText={setPassword} placeholder="Password" style={styles.input}/>
       { loading ? <ActivityIndicator size="large" color='#0000ff'/>
