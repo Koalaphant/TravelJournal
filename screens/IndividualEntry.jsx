b1312e646d3b3380275e92403e7910aa712cc68b
Koalaphant <andrew.wardjones@icloud.com>
2024-03-27 16:48:18 +0000

    added user context to pass UID to different parts of app

--- screens/IndividualEntry.jsx
+++ screens/IndividualEntry.jsx
@@ -1,36 +1,48 @@
-import { View, Text, StyleSheet, ScrollView, TouchableOpacity } from "react-native";
-import React, { useState, useEffect } from "react";
-import { getDoc,doc, collection } from "firebase/firestore";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState, useEffect, useContext } from "react";
+import { getDoc, doc, collection } from "firebase/firestore";
 import { db } from "../services/config.js";
+import { UserContext } from "../App.js";
 
-const IndividualEntry = ({route}) => {
+const IndividualEntry = ({ route }) => {
   const [journal, setJournal] = useState({});
-  const entry = route.params.params
-    console.log(entry, " <<PROPS");
+
+  const user = useContext(UserContext);
+
+  console.log("This is the current user's logged in ID:" + " " + user.uid);
+
+  const entry = route.params.params;
+  console.log(entry, " <<PROPS");
   useEffect(() => {
-  getDoc(doc(db, "Traveller2", 'England', 'Trip1', entry)).then((data) =>{
-    if(data.exists()){
-        setJournal(data.data())
-    }
-}).catch((error) =>{
-    console.log(error)
-})
-}, []);
+    getDoc(doc(db, "Traveller2", "England", "Trip1", entry))
+      .then((data) => {
+        if (data.exists()) {
+          setJournal(data.data());
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   return (
-   
-                <View style={styles.wholeScreen}>
-                <Text style={styles.header}>{journal.title}</Text>
-                <Text style={styles.box}>Location: {journal.location}</Text>
-                <Text style={styles.box}>Details: {journal.journalentry}</Text>
-                </View>
-     
+    <View style={styles.wholeScreen}>
+      <Text style={styles.header}>{journal.title}</Text>
+      <Text style={styles.box}>Location: {journal.location}</Text>
+      <Text style={styles.box}>Details: {journal.journalentry}</Text>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   wholeScreen: {
-    backgroundColor: '#FFEDDF'
+    backgroundColor: "#FFEDDF",
   },
   header: {
     backgroundColor: "#D76778",
@@ -39,7 +51,7 @@ const styles = StyleSheet.create({
     fontSize: 30,
     marginHorizontal: 10,
     marginVertical: 10,
-    borderRadius: 8
+    borderRadius: 8,
   },
   box: {
     backgroundColor: "#FFEDDF",
@@ -48,7 +60,7 @@ const styles = StyleSheet.create({
     fontSize: 20,
     marginHorizontal: 10,
     marginVertical: 10,
-    borderRadius: 8
+    borderRadius: 8,
   },
   box1: {
     backgroundColor: "violet",
