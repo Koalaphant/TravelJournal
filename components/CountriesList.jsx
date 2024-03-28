1dfb5c08fc49a5c53934115dfcdbd4f435dcc818
Koalaphant <andrew.wardjones@icloud.com>
2024-03-28 13:55:11 +0000

    specific content for each user

--- components/CountriesList.jsx
+++ components/CountriesList.jsx
@@ -1,50 +1,51 @@
-import React from "react";
-import { Text, View, StyleSheet, ImageBackground } from "react-native";
+import React, { useState, useEffect, useContext } from "react";
+import { Text, View, StyleSheet } from "react-native";
 import { useFonts, Poppins_400Regular } from "@expo-google-fonts/poppins";
+import { getDocs, collection, query, where } from "firebase/firestore";
+import { db } from "../services/config.js";
+import { UserContext } from "../App.js";
 
 const CountriesList = () => {
   let [fontsLoaded] = useFonts({
     Poppins_400Regular,
   });
 
-  if (!fontsLoaded) {
-    return null; // or a loading indicator
-  }
+  const user = useContext(UserContext); // Access user context
 
-  const testData = [
-    {
-      cityName: "Paris",
-      coverPhoto:
-        "https://images.pexels.com/photos/2363/france-landmark-lights-night.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    },
-    {
-      cityName: "Berlin",
-      coverPhoto:
-        "https://images.pexels.com/photos/109630/pexels-photo-109630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    },
-    {
-      cityName: "Milan",
-      coverPhoto:
-        "https://images.pexels.com/photos/37079/milan-pegasus-gallery-statue.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    },
-    {
-      cityName: "London",
-      coverPhoto:
-        "https://images.pexels.com/photos/460672/pexels-photo-460672.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    },
-  ];
+  const [entries, setEntries] = useState([]);
+
+  useEffect(() => {
+    const fetchEntries = async () => {
+      try {
+        if (!user) return; // If user context is not available, return
+
+        const entriesRef = collection(db, "Entries");
+        const q = query(entriesRef, where("UID", "==", user.uid)); // Filter entries by user's UID
+        const querySnapshot = await getDocs(q);
+
+        const entriesData = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+
+        setEntries(entriesData);
+      } catch (error) {
+        console.error("Error fetching entries:", error);
+      }
+    };
+
+    fetchEntries();
+  }, [user]);
+
+  if (!fontsLoaded || entries.length === 0) {
+    return null;
+  }
 
   return (
     <View style={styles.container}>
-      {testData.map((item, index) => (
+      {entries.map((item, index) => (
         <View key={index} style={styles.itemContainer}>
-          <ImageBackground
-            source={{ uri: item.coverPhoto }}
-            style={styles.imageBackground}
-          >
-            <View style={styles.overlay} />
-            <Text style={styles.text}>{item.cityName}</Text>
-          </ImageBackground>
+          <Text style={styles.text}>{item.country}</Text>
         </View>
       ))}
     </View>
@@ -69,26 +70,14 @@ const styles = StyleSheet.create({
     shadowRadius: 3.84,
     elevation: 5,
     width: "100%",
-    height: 200,
-  },
-  imageBackground: {
-    width: "100%",
-    height: "100%",
+    height: 50,
     justifyContent: "center",
-    alignItems: "center",
-  },
-  overlay: {
-    ...StyleSheet.absoluteFillObject,
-    backgroundColor: "rgba(0, 0, 0, 0.3)", // Adjust the opacity as needed
-    borderRadius: 10,
+    backgroundColor: "#fff",
   },
   text: {
     fontFamily: "Poppins_400Regular",
-    fontSize: 40,
-    color: "white",
-    textShadowColor: "rgba(0, 0, 0, 0.75)", // Shadow color
-    textShadowOffset: { width: 2, height: 2 }, // Shadow offset
-    textShadowRadius: 10, // Shadow blur radius
+    fontSize: 20,
+    color: "#000",
   },
 });
 
